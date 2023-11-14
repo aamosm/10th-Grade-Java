7aaba209ed016d2aa51cd34853a250bf30a36067
aamosm <109422037+aamosm@users.noreply.github.com>
2023-11-14 13:15:01 +0530

    Create Sum_HiestNum.java

---
+++ school/Sum_HiestNum.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class prog3 extends MIDTERM_LIST{
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        Double a[]= new Double[10];
+
+        System.out.println("Input the numbers : ");
+        for (int w = 0;w<10;w++){
+
+            a[w] = sc.nextDouble();
+
+        }
+        Double sum = 0d;
+        Double most= 0d;
+
+        
+        for (int w = 0;w<10;w++){
+
+            sum = sum + a[w];
+
+        }
+        for (int i = 0;i<10;i++){
+            if (a[i]>most){
+                most = a[i] ;
+            }
+        } 
+        System.out.println("The sum is "+sum);
+        System.out.println("The highest number is "+most);
+
+    }
+}
